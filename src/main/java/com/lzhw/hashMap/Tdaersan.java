b66823844fdaa67dec06b21a26dde72c5bea8a3e
daersanpao <lv996074344zhi@163.com>
2021-07-06 15:36:12 +0800

    Create Tdaersan.java

---
+++ src/main/java/com/lzhw/hashMap/Tdaersan.java
@@ -0,0 +1,12 @@
+package com.lzhw.hashMap;
+public class Tdaersan {
+
+
+    public void add(int i) {
+        int a = 1 / 0;
+        String a1 = null;
+        String b = a1.toString();
+
+    }
+
+}
