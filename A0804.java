d2dc48659da724b7266a880f8424d49ee2e446e6
daersanpao <lv996074344zhi@163.com>
2021-07-28 15:26:03 +0800

    Update A0804.java

--- A0804.java
+++ A0804.java
@@ -2,4 +2,11 @@ class A084{
   public static void main(String [] args){
     int a=1/0;
   }
+  
+   public void add(int i) {
+        int a = 1 / 0;
+        String a1 = null;
+        String b = a1.toString();
+         int a3 = 1 / 0;
+    }
 }
