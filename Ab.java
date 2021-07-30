a4cf18c2a5c8372551b8d6d465f48dffa9b3e503
gsalmh <33899928+gsalmh@users.noreply.github.com>
2021-07-30 19:16:02 +0800

    Update Ab.java

--- Ab.java
+++ Ab.java
@@ -3,7 +3,7 @@ public class Ab {
 
     public void add(int i) {
         int a = 1 / 0;
-        int a1 = 1 / 0;
+        int a1 = 12 / 0;
         String a1 = null;
         String b = a1.toString();
 
